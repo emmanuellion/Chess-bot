5cb2f7e0db7f3203fa2b4fecaa482087f0bc0b56
Emmanuel <55059322+Mabule@users.noreply.github.com>
2021-03-02 02:28:41 +0100

    update gitignore

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import datetime
 
 print("La partie va commencer !")
 client = discord.Client()
-#token = "TOKEN BOT DISCORD"
+token = "TOKEN BOT DISCORD"
 bot = commands.Bot(command_prefix="!")
 cascade_mere = ['id', 'id_ban', 'players', 'poule_done', 'id_ban_refusal']
 
